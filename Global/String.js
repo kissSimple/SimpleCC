4ee0be2f82467a31c5b3a4f1ce7a3fdd7af64293
hujf <41710215+kissSimple@users.noreply.github.com>
2019-03-04 19:14:34 +0800

    提示文字
    海外版的时候替换这里的文字就可以了

---
+++ Global/String.js
@@ -0,0 +1,26 @@
+// 提示文字   海外版的时候替换这里的文字就可以了
+
+
+window.g_string = {
+    shareTimeMax: "分享次数已达上限",
+    shareSuccess: "分享成功",
+    shareFailed: "分享失败，换一个好友/群试下",
+
+    videoTimeMax: "观看视频次数已达上限",
+    watchAdSuccess: "观看视频广告成功",
+    noVideoAd : "暂无视频广告观看",
+    videoUnfinished : "播放中途退出，无法获取奖励",
+    adFailed : "获取广告失败",
+    noAd : "无适合的广告",
+    adUnitClosed : "广告单元已关闭",
+
+    diamondNotEnough: "水晶不够呀，兄dei",
+    adReviveTimeMax : "当前关卡看视频复活次数已到达上限",
+
+    wxLoginFailed: '登录失败 wx.login',
+    loginFailed: '登录失败',
+
+    alreadyReceived : '奖励已经领取过了,请不要重复领取哦',
+    receiveSuccess : '奖励领取成功',
+    gameTimeNotEnough : '请体验15秒以上才能领取奖励哟'
+};
