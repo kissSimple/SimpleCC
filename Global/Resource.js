186f4984f41a974043fedbc90c1ba116554b6545
hujf <41710215+kissSimple@users.noreply.github.com>
2019-03-04 19:12:43 +0800

    资源的路径

---
+++ Global/Resource.js
@@ -0,0 +1,22 @@
+// 资源的路径
+
+
+window.g_resource = {
+    // 数据表
+    tablePath: "tables",// 数据表目录
+    mapPath: "map/",// 关卡数据目录
+    levelTable : "tables/level", // 关卡数据表
+    guideTable: "tables/guide",    //导航表
+    gamePlayTable: "tables/JumpReward",        //小游戏
+
+    // 音效
+    hallBgMusic : "audio/bgMusic/menuUiMusic", //
+    fightBgMusic : "audio/bgMusic/backMusic", //
+    winAudio : "audio/win", //
+    failAudio : "audio/fail", //
+
+    // 界面预制体
+    mainPanel: "panel/mainPanel/mainPanel",//大厅主界面
+    guidePanel : "panel/guideUI/GuideUI",// 新手引导
+
+};
